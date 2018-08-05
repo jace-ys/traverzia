01c18a8461c365e9393a3ecad7c5f47b6f41c4ea
Jaceys <jaceys.tan@gmail.com>
2018-08-05 23:13:32 +0100

    updated app.js with body-parser

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 // Require dependencies
-var express = require('express'),
+var path = require("path"),
+	bodyParser = require("body-parser"),
+	express = require("express"),
 	app = express();
 
+// Setup
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
 // Define port for server to listen on
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 var port = app.get('port');
 
-// Routes
-app.get('/', (req, res) => {
-	res.send("Welcome to Traverzia");
+// GET routes
+app.get("/", (req, res) => {
+	res.render("home");
+})
+
+// Handle invalid routes
+app.get("/error", (req, res) => {
+	res.send("404 page not found!");
+})
+
+app.get("*", (req, res) => {
+	res.redirect("/error");
 })
 
 // Listen
