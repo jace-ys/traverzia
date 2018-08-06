68cc1657f1b7e9dc2a0465e28080718ecf97fe3b
Jaceys <jaceys.tan@gmail.com>
2018-08-06 16:39:49 +0100

    added images

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// Data
+var user = "Jace";
+var data = [
+	{
+		title: "image1",
+		image_url: "/images/travelogue_1.jpg"
+	},
+	{
+		title: "image2",
+		image_url: "/images/travelogue_2.jpg"
+	}
+];
+
 // Define port for server to listen on
 app.set("port", process.env.PORT || 3000);
 var port = app.get('port');
@@ -16,6 +29,15 @@ var port = app.get('port');
 // GET routes
 app.get("/", (req, res) => {
 	res.render("home");
+})
+
+app.get("/search", (req, res) => {
+	var search = req.query.search;
+	res.render("user", {user: user, imageData: data});
+})
+
+app.get("/user", (req, res) => {
+	res.render("user", {user: user});
 })
 
 // Handle invalid routes
