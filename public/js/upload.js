6562cc9df1593f3ecd1a2da75a281cf5b1a3fcd4
Jaceys <jaceys.tan@gmail.com>
2018-08-14 00:10:03 +0100

    parse location

---
+++ public/js/upload.js
@@ -0,0 +1,28 @@
+$("#upload-form").on("submit", (event) => {
+	event.preventDefault();
+	var locationInput = $("#upload-form input[name='image[location]']").val();
+	var parsedInput = locationInput.split(/, /);
+	var location = parsedInput[0];
+	var country = parsedInput[1];
+	var endpoint = $("#upload-form").attr("action");
+	var newImage = {
+		source: $("#upload-form input[name='image[source]']").val(),
+		country: country,
+		location: location,
+		caption: $("#upload-form textarea").val()
+	}
+	$.ajax({
+		type: "POST",
+        url: endpoint,
+        data: newImage,
+        dataType: "json",
+        success: function(res) {
+        	if(res.redirect_url) {
+            	window.location.href = res.redirect_url;
+        	}
+        },
+        error: function(res) {
+            console.log("Failed to upload");
+        }
+	});
+});
