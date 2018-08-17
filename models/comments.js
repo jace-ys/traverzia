2090df7e1f2d3949819017d2c61e94a8ed08a16a
Jaceys <jaceys.tan@gmail.com>
2018-08-17 02:09:52 +0100

    to fix delete

--- models/comments.js
+++ models/comments.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 var commentSchema = new mongoose.Schema({
 	text: String,
-	author: String
+	author: String,
+	image: String,
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
