e1c9fdd9f3794a99667a3db10bb3128cf15a5d95
Jaceys <jaceys.tan@gmail.com>
2018-09-10 18:09:48 +0800

    cache control

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.use(require("express-session")({
 	resave: false,
 	saveUninitialized: false
 }));
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0");
+  next();
+});
 app.use(flash());
 
 // MongoDB setup
